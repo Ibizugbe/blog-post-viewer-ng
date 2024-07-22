e08f186f37d2ed14cbd1db4c2504a8a08ac3628e
ibizugbe <abrahamibizugbe@gmail.com>
2024-07-22 18:40:18 +0100

    Configure app module correctly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blog-post-viewer-ng';
+  title = 'blog-post-viewer';
 }
